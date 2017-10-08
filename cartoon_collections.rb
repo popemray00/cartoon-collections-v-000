5824df7138202452fa4711a37fbd6eaacf834a12
Michael Pope <popemray1986@gmail.com>
2017-10-08 18:43:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
+    cheddar_cheese.find do |type|
     cheese_types.include?(type)
   end
 end
